cfe3bf5b093ba3d0d6f22adecb087fc599b96430
Tomas Kraus <tomas.kraus.dev@gmail.com>
2023-02-04 21:56:40 +0100

    feat: new HeadTailMatcher is ready

---
+++ build/test/utils/head_tail_matcher.test.js
@@ -0,0 +1,70 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const head_tail_matcher_1 = require("../../src/utils/head_tail_matcher");
+let spaceMatcher;
+let pipeMatcher;
+beforeEach(() => {
+    spaceMatcher = (0, head_tail_matcher_1.createHeadTailMatcher)(' ');
+    pipeMatcher = (0, head_tail_matcher_1.createHeadTailMatcher)('\\|');
+});
+describe('special regexp chars in separator string', () => {
+    test('need to double escape special char meaning', () => {
+        const dotMatcher = (0, head_tail_matcher_1.createHeadTailMatcher)('\\.');
+        expect(dotMatcher.headTail('a.b.c')).toEqual(['a', 'b.c']);
+        expect(dotMatcher.head('a.b.c')).toEqual('a');
+        expect(dotMatcher.tail('a.b.c')).toEqual('b.c');
+    });
+});
+describe('HeadTailMatcher', () => {
+    test('on empty string, returns empty head and empty tail', () => {
+        expect(spaceMatcher.headTail('')).toEqual(['', '']);
+        expect(spaceMatcher.head('')).toEqual('');
+        expect(spaceMatcher.tail('')).toEqual('');
+        expect(pipeMatcher.headTail('')).toEqual(['', '']);
+        expect(pipeMatcher.head('')).toEqual('');
+        expect(pipeMatcher.tail('')).toEqual('');
+    });
+    test('on string that contains only one separator, returns empty head and empty tail', () => {
+        expect(spaceMatcher.headTail(' ')).toEqual(['', '']);
+        expect(spaceMatcher.head(' ')).toEqual('');
+        expect(spaceMatcher.tail(' ')).toEqual('');
+        expect(pipeMatcher.headTail('|')).toEqual(['', '']);
+        expect(pipeMatcher.head('|')).toEqual('');
+        expect(pipeMatcher.tail('|')).toEqual('');
+    });
+    test('removes leading white spaces first. Even if white space is a separator!', () => {
+        expect(spaceMatcher.headTail('   ')).toEqual(['', '']);
+        expect(spaceMatcher.head('   ')).toEqual('');
+        expect(spaceMatcher.tail('   ')).toEqual('');
+        expect(pipeMatcher.headTail('   | ')).toEqual(['', ' ']);
+        expect(pipeMatcher.head('   | ')).toEqual('');
+        expect(pipeMatcher.tail('   | ')).toEqual(' ');
+    });
+    test('trims the head (even if a separator is a white space!)', () => {
+        expect(spaceMatcher.head(' hello  ')).toEqual('hello');
+        expect(pipeMatcher.head(' hello  ')).toEqual('hello');
+        expect(pipeMatcher.head('   | abc ')).toEqual('');
+    });
+    test('if separator is white char, removes leading white spaces from the tail)', () => {
+        expect(spaceMatcher.headTail(' hello  our world ')).toEqual([
+            'hello',
+            'our world ',
+        ]);
+        expect(spaceMatcher.head(' hello  our world ')).toEqual('hello');
+        expect(spaceMatcher.tail(' hello  our world ')).toEqual('our world ');
+    });
+    test('tail is the rest of the original string', () => {
+        expect(pipeMatcher.tail(' hello  | our  world ')).toEqual(' our  world ');
+        expect(pipeMatcher.tail(' hello|')).toEqual('');
+        expect(spaceMatcher.tail(' hello  | our  world ')).toEqual('| our  world ');
+    });
+    test('consumes the first (non white char) separator occurence (removes leading white spaces first!)', () => {
+        expect(pipeMatcher.headTail('  hello | our world | ! ')).toEqual([
+            'hello',
+            ' our world | ! ',
+        ]);
+        expect(pipeMatcher.head('  hello | our world | ! ')).toEqual('hello');
+        expect(pipeMatcher.tail('  hello | our world | ! ')).toEqual(' our world | ! ');
+    });
+});
+//# sourceMappingURL=head_tail_matcher.test.js.map
---
+++ test/utils/head_tail_matcher.test.ts
@@ -0,0 +1,88 @@
+import {
+  IHeadTailMatcher,
+  createHeadTailMatcher,
+} from '../../src/utils/head_tail_matcher';
+
+let spaceMatcher: IHeadTailMatcher;
+let pipeMatcher: IHeadTailMatcher;
+
+beforeEach(() => {
+  spaceMatcher = createHeadTailMatcher(' ');
+  pipeMatcher = createHeadTailMatcher('\\|');
+});
+
+describe('special regexp chars in separator string', () => {
+  test('need to double escape special char meaning', () => {
+    const dotMatcher = createHeadTailMatcher('\\.');
+
+    expect(dotMatcher.headTail('a.b.c')).toEqual(['a', 'b.c']);
+    expect(dotMatcher.head('a.b.c')).toEqual('a');
+    expect(dotMatcher.tail('a.b.c')).toEqual('b.c');
+  });
+});
+
+describe('HeadTailMatcher', () => {
+  test('on empty string, returns empty head and empty tail', () => {
+    expect(spaceMatcher.headTail('')).toEqual(['', '']);
+    expect(spaceMatcher.head('')).toEqual('');
+    expect(spaceMatcher.tail('')).toEqual('');
+
+    expect(pipeMatcher.headTail('')).toEqual(['', '']);
+    expect(pipeMatcher.head('')).toEqual('');
+    expect(pipeMatcher.tail('')).toEqual('');
+  });
+
+  test('on string that contains only one separator, returns empty head and empty tail', () => {
+    expect(spaceMatcher.headTail(' ')).toEqual(['', '']);
+    expect(spaceMatcher.head(' ')).toEqual('');
+    expect(spaceMatcher.tail(' ')).toEqual('');
+
+    expect(pipeMatcher.headTail('|')).toEqual(['', '']);
+    expect(pipeMatcher.head('|')).toEqual('');
+    expect(pipeMatcher.tail('|')).toEqual('');
+  });
+
+  test('removes leading white spaces first. Even if white space is a separator!', () => {
+    expect(spaceMatcher.headTail('   ')).toEqual(['', '']);
+    expect(spaceMatcher.head('   ')).toEqual('');
+    expect(spaceMatcher.tail('   ')).toEqual('');
+
+    expect(pipeMatcher.headTail('   | ')).toEqual(['', ' ']);
+    expect(pipeMatcher.head('   | ')).toEqual('');
+    expect(pipeMatcher.tail('   | ')).toEqual(' ');
+  });
+
+  test('trims the head (even if a separator is a white space!)', () => {
+    expect(spaceMatcher.head(' hello  ')).toEqual('hello');
+
+    expect(pipeMatcher.head(' hello  ')).toEqual('hello');
+    expect(pipeMatcher.head('   | abc ')).toEqual('');
+  });
+
+  test('if separator is white char, removes leading white spaces from the tail)', () => {
+    expect(spaceMatcher.headTail(' hello  our world ')).toEqual([
+      'hello',
+      'our world ',
+    ]);
+    expect(spaceMatcher.head(' hello  our world ')).toEqual('hello');
+    expect(spaceMatcher.tail(' hello  our world ')).toEqual('our world ');
+  });
+
+  test('tail is the rest of the original string', () => {
+    expect(pipeMatcher.tail(' hello  | our  world ')).toEqual(' our  world ');
+    expect(pipeMatcher.tail(' hello|')).toEqual('');
+
+    expect(spaceMatcher.tail(' hello  | our  world ')).toEqual('| our  world ');
+  });
+
+  test('consumes the first (non white char) separator occurence (removes leading white spaces first!)', () => {
+    expect(pipeMatcher.headTail('  hello | our world | ! ')).toEqual([
+      'hello',
+      ' our world | ! ',
+    ]);
+    expect(pipeMatcher.head('  hello | our world | ! ')).toEqual('hello');
+    expect(pipeMatcher.tail('  hello | our world | ! ')).toEqual(
+      ' our world | ! '
+    );
+  });
+});
