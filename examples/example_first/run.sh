649177316cd4e1ee9f683603088980dab82248f9
Tomas Kraus <tomas.kraus.dev@gmail.com>
2023-03-30 01:44:31 +0200

    fix(docs): wrong usage example

--- examples/example_first/run.sh
+++ examples/example_first/run.sh
@@ -4,6 +4,6 @@ node ../../bin/cli -i README.template.md > README.md
 
 if (( 0 )) ; then
 #< Generate a documentation with the updated information:
-npx includio --i README.template.md > README.md
+npx includio -i README.template.md > README.md
 #<
 fi
